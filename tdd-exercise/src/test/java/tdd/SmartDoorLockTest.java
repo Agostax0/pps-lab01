2182b041b6029fb09b0fc519828adcd26fbbe9ca
alessandro.agosta <alessandro.agosta@studio.unibo.it>
2025-02-24 10:46:06 +0100

    initially unlocked and unblocked

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,26 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+    private SmartDoorLock smartDoorLock;
+
+    @BeforeEach
+    void beforeEach(){
+        this.smartDoorLock = new SmartDoorLockImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void isInitiallyUnlocked() {
+        assertFalse(smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void isInitiallyNotBlocked(){
+        assertFalse(smartDoorLock.isBlocked());
     }
 }
