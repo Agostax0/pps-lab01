9da72f5e2305c47b8dccfcc024e8bad7c4c36ecc
alessandro.agosta <alessandro.agosta@studio.unibo.it>
2025-02-25 20:21:54 +0000

    start

--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -1,10 +1,19 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+
+    private MinMaxStack stack;
+
+    @BeforeEach
+    public void beforeEach(){
+        this.stack = new MinMaxStackImpl();
+    }
+
     @Test
     public void todo() {
         assertTrue(true);
