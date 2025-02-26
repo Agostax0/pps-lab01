1c9163ac0f42c17d27a848038e21709759c10015
alessandro.agosta <alessandro.agosta@studio.unibo.it>
2025-02-26 13:26:28 +0000

    size and isEmpty queue

--- tdd-exercise/src/test/java/tdd/CircularListTest.java
+++ tdd-exercise/src/test/java/tdd/CircularListTest.java
@@ -1,16 +1,29 @@
 package tdd;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
-    @Test
-    public void todo() {
-        assertTrue(true);
+
+    private static final int QUEUE_INITIAL_SIZE = 0;
+    private CircularQueue queue;
+
+    @BeforeEach
+    public void beforeEach(){
+
     }
+
+    @Test
+    public void isInitiallyEmpty() {
+        assertTrue(this.queue.isEmpty());
+    }
+
+    public void isSizeInitiallyZero(){assertEquals(QUEUE_INITIAL_SIZE, this.queue.size());}
 }
